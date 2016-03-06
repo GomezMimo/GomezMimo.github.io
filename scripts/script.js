e2d56c3e61f56f614245e6f850b77cfc015c88e0
Juan Gomez <juan.camilo.mimo@gmail.com>
2016-03-06 03:03:15 -0500

    Add reactor quimico javascript project

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+/*Variables*/
+var A = 0.281; // A es igual a C(0)
+var B = 5; // B es igual a C(ent)
+var a = 38;
+var b = 0.721;
+var d = 0.92;
+var e = 2.71828182;
+var t1, t2, ct1, ct2, m, b, t3, ct3, b2;
+
+/*Variables html*/
+var html = "";
+
+var contenedor = document.getElementById("contenedor");
+
+
+
+/*Botones*/
+document.getElementById("secante").addEventListener("click", function(){
+	html = "<table> <tr><th>n</th><th>t1</th><th>c(t1)</th><th>t2</th><th>c(t2)</th><th>m</th><th>b2</th><th>t3</th><th>c(t3)</th></tr>";
+	biseccion();
+	
+});
+
+
+function biseccion(){
+	var contador = 1;
+	t1 = 0;
+	t2 = 2;
+	ct3 = 1;		
+	while(ct3 != 0){		
+		ct1 = ( B * ( 1 - Math.pow(e, -a * t1)) + A * Math.pow(e,-b*t1) - 0.45 * B);
+		ct2 = ( B * ( 1 - Math.pow(e, -a * t2)) + A * Math.pow(e,-b*t2) - 0.45 * B);
+		m = ((ct2 - ct1) / (t2 - t1));		
+		b2 = (-m*t1) + ct1;		
+		t3 = (-b2 / m);
+		ct3 = (( B * ( 1 - Math.pow(e, -a * t3)) + A * Math.pow(e,-b*t3) - 0.45 * B));			
+			if(ct1 * ct3 < 0){					
+				ct1 = ct1;
+			}else{				
+				ct1 = ct3;
+			}		
+			if(ct2 * ct3 < 0){				
+				ct1 = ct1;
+			}else{				
+				t2 = t3;				
+			}	
+				
+		html += "<tr><td>"+ (contador) +"</td><td>"+ t1 +"</td><td>" + ct1.toFixed(4) +"</td><td>"+ t2.toFixed(4) +"</td><td>"+ ct2.toFixed(16) +"</td><td>"+ m.toFixed(4) +"</td><td>" + b2.toFixed(4) + "</td><td>"+ t3.toFixed(4) +"</td><td>"+ ct3.toFixed(16) +"</td></tr>"
+		contador++;
+	}	
+	return contenedor.innerHTML = html + "</table>";
+}
