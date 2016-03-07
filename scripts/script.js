b1fe47f23389998916d80833f439dcd1602dac84
Juan Gomez <juan.camilo.mimo@gmail.com>
2016-03-06 22:49:37 -0500

    Add bisecction method

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ var a = 38;
 var b = 0.721;
 var d = 0.92;
 var e = 2.71828182;
-var t1, t2, ct1, ct2, m, b, t3, ct3, b2;
+var ct1, ct2, m, b, t3, ct3, b2;
 
 /*Variables html*/
 var html = "";
@@ -17,22 +17,31 @@ var contenedor = document.getElementById("contenedor");
 /*Botones*/
 document.getElementById("secante_1").addEventListener("click", function(){
 	html = "<table> <tr><th>n</th><th>t1</th><th>c(t1)</th><th>t2</th><th>c(t2)</th><th>m</th><th>b2</th><th>t3</th><th>c(t3)</th></tr>";
-	biseccion(0, 2, 0.45);
+	secante(0, 2, 0.45);
 	
 });
 
-document.getElementById("secante_2").addEventListener("click", function(){
+document.getElementById("secante_2").addEventListener("click", function(){	
 	html = "<table> <tr><th>n</th><th>t1</th><th>c(t1)</th><th>t2</th><th>c(t2)</th><th>m</th><th>b2</th><th>t3</th><th>c(t3)</th></tr>";
-	biseccion(0, 1, 0.92);
+	secante(0, 1, 0.92);
 	
 });
 
+document.getElementById("biseccion_1").addEventListener("click", function(){	
+	html = "<table><tr><th>n</th><th>t1</th><th>c(t1)</th><th>t2</th><th>c(t2)</th><th>t3</th><th>c(t3)</th></tr>";
+	biseccion(0, 2, 0.45);
+});
 
-function biseccion(t1, t2, cx){
+document.getElementById("biseccion_2").addEventListener("click", function(){	
+	html = "<table><tr><th>n</th><th>t1</th><th>c(t1)</th><th>t2</th><th>c(t2)</th><th>t3</th><th>c(t3)</th></tr>";
+	biseccion(0, 1, 0.92);
+});
+
+/*Función de bisección*/
+function secante(t1, t2, cx){	
 	var contador = 1;		
 	ct1 = 1;
-	ct2 = 1;		
-	alert(ct1 +" "+ ct2);
+	ct2 = 1;			
 	while(ct1 != 0 || ct2 != 0){				
 		ct1 = ( B * ( 1 - Math.pow(e, -a * t1)) + A * Math.pow(e,-b*t1) - cx * B);
 		ct2 = ( B * ( 1 - Math.pow(e, -a * t2)) + A * Math.pow(e,-b*t2) - cx * B);
@@ -58,3 +67,38 @@ function biseccion(t1, t2, cx){
 	}	
 	return contenedor.innerHTML = html + "</table>";
 }
+
+/*Función de bisección*/
+
+function biseccion(t1, t2, cx){	
+	var contador = 1;		
+	ct1 = 1;
+	ct2 = 1;			
+	while(ct2 != 0){				
+		ct1 = ( B * ( 1 - Math.pow(e, -a * t1)) + A * Math.pow(e,-b*t1) - cx * B);
+		ct2 = ( B * ( 1 - Math.pow(e, -a * t2)) + A * Math.pow(e,-b*t2) - cx * B);			
+		t3 = (t1+t2)/2;
+		html += "<tr><td>"+ (contador) +"</td><td>"+ t1.toFixed(4) +"</td><td>" + ct1 +"</td><td>"+ t2.toFixed(4) +"</td><td>"+ ct2.toFixed(16) +"</td><td>"+ t3.toFixed(10) +"</td>";
+		contador++;
+		ct3 = (( B * ( 1 - Math.pow(e, -a * t3)) + A * Math.pow(e,-b*t3) - cx * B));			
+			if(ct3 < 0) {
+				t1 = t3;
+			}else{
+				t2 = t3;
+			}	
+		html+= "<td>"+ ct3.toFixed(16) +"</td></tr>";		
+		
+	}	
+	return contenedor.innerHTML = html + "</table>";
+}
+
+/*Función de Newton Rapshon*/
+
+
+
+
+
+
+
+
+
